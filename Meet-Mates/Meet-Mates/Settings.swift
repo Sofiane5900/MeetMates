c6d6c7ebb5f938e4f5c022ca6e8ed05774d2fa9d
Raigaa <132461783+Raigaa@users.noreply.github.com>
2023-05-03 14:53:10 +0200

    Add files via upload

---
+++ Meet-Mates/Meet-Mates/Settings.swift
@@ -0,0 +1,30 @@
+//
+//  Settings.swift
+//  Meet-Mates
+//
+//  Created by apprenant62 on 03/05/2023.
+//
+
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        ZStack{
+            Color("customGray")
+                .ignoresSafeArea()
+            VStack{
+                Image(user1.userImage)
+                    .resizable()
+                    .frame(width: 150,height: 150)
+                    .clipShape(Circle())
+                Spacer()
+            }
+        }
+    }
+}
+
+struct Settings_Previews: PreviewProvider {
+    static var previews: some View {
+        Settings()
+    }
+}
